7090c7e3913849ddb97ba9b7808ad30cb16d8ca0
Ashley Levy <alevy@gapintelligence.com>
2017-11-01 15:10:05 -0700

    Finding webpage

--- crawler.rb
+++ crawler.rb
@@ -1,12 +1,20 @@
-class Crawler
-  def crawl
-  	entry = BlogEntry.new
-  	entry.author = "Dr. Seuss"
-  	entry.title = "The Cat in the Hat"
-  	entry.shares = "23"
+require 'nokogiri'
+require 'mechanize'
 
-  	display_name = "#{entry.title}, By: #{entry.author}"
-  	puts display_name
+class Crawler
+  def crawl(url)
+  	agent = Mechanize.new
+  	agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#  	entry = BlogEntry.new
+#  	entry.author = "Dr. Seuss"
+#  	entry.title = "The Cat in the Hat"
+#  	entry.shares = "23"
+
+#  	display_name = "#{entry.title}, By: #{entry.author}"
+#  	puts display_name
+
+	page = agent.get(url)
+	puts page.uri
   end
 end
 
